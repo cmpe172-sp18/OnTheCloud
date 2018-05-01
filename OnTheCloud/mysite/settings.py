4441b7bc12e33a494dbe0f15d2446ff6380969ef
mengchhay <34071332+mengchhay@users.noreply.github.com>
2018-04-30 22:31:11 -0700

    Update settings.py

--- OnTheCloud/mysite/settings.py
+++ OnTheCloud/mysite/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9tf$jps6u-rxnv8nuur=*z&44$d!*_k@9td4jfaurtd5)xu_50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
