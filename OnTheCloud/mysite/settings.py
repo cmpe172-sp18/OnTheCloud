92ed6aae79e5916cbc2b6aba686db1c3e79638a4
Hovanes Bakchagyan <hovanes.bakchagyan@gmail.com>
2018-04-30 19:10:55 -0700

    Update settings.py

--- OnTheCloud/mysite/settings.py
+++ OnTheCloud/mysite/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '9tf$jps6u-rxnv8nuur=*z&44$d!*_k@9td4jfaurtd5)xu_50'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
